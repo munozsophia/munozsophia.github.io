8366ae4a52b9c129bb251fbcc36448bed73c73b8
Sophia Munoz <munozsa@mail.uc.edu>
2026-06-14 14:41:10 -0400

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/munozsophia.github.io/',
+    base: '/',
     plugins: [react()],
     build: {
         rollupOptions: {
